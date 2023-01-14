7c6a5130683f4aada2262380834ff004f8dfe1d0
DiwakarSinghRajput <diwakarrocks92@gmail.com>
2023-01-14 23:07:05 +0530

    dummy entry added for the budget allocation

--- src/app/budget-allocation/budget-allocation.component.ts
+++ src/app/budget-allocation/budget-allocation.component.ts
@@ -13,19 +13,59 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class BudgetAllocationComponent implements OnInit {
   selectedProjects: any;
 
-  budgetFinYears: any[] = [];
-  subHeads: any[] = [];
+  // budgetFinYears: any[] = [];
+  // subHeads: any[] = [];
   // cgUnits: any[] = [];
-  budgetTypes: any[] = [];
-  allocationTypes: any[] = [];
+  // budgetTypes: any[] = [];
+  // allocationTypes: any[] = [];
   majorHead: any;
   minorHead: any;
   fundAvailable: any;
   previousAllocation: any;
+  balanceFund: any;
+
+  budgetFinYears = [
+    { serialNo: 1, finYear: '2020-2021' },
+    { serialNo: 2, finYear: '2021-2022' },
+    { serialNo: 3, finYear: '2022-2023' },
+  ];
+
+  subHeads = [
+    {
+      id: 1,
+      subheadDesc: 'Office Convinece',
+      majorHead: '123',
+      minorHead: '123',
+    },
+    {
+      id: 2,
+      subheadDesc: 'Room Convinece',
+      majorHead: '123',
+      minorHead: '123',
+    },
+    {
+      id: 3,
+      subheadDesc: 'Party Convinece',
+      majorHead: '123',
+      minorHead: '123',
+    },
+  ];
 
   cgUnits = [
     { unit: 456, descr: 'CGIO Okha' },
     { unit: 457, descr: 'ICGS Delhi' },
+  ];
+
+  budgetTypes = [
+    { id: 1, descr: 'demo1' },
+    { id: 2, descr: 'demo2' },
+    { id: 3, descr: 'demo3' },
+  ];
+
+  allocationTypes = [
+    { id: 1, allocDesc: 'demo1' },
+    { id: 2, allocDesc: 'demo2' },
+    { id: 3, allocDesc: 'demo3' },
   ];
 
   constructor(
@@ -36,6 +76,7 @@ export class BudgetAllocationComponent implements OnInit {
 
   ngOnInit(): void {
     this.getDataBudgetAllocation();
+    this.balanceFund = 0;
     // this.getCombo("MONTH", "");
     // this.getCombo("YEAR", "");
     // this.cgwwaUserDetails = JSON.parse(
@@ -73,6 +114,11 @@ export class BudgetAllocationComponent implements OnInit {
   }
 
   getAvailableFund(cgUnit: any) {
+    // Only for demo
+    this.fundAvailable = '10000';
+    this.previousAllocation = '2000';
+
+    // Only for demo
     this.SpinnerService.show();
     var comboJson = null;
     console.log(JSON.stringify(comboJson) + ' ======');
@@ -88,4 +134,12 @@ export class BudgetAllocationComponent implements OnInit {
   }
 
   addData() {}
+
+  onSearchChange(searchValue: string): void {
+    if (Number(searchValue) > this.fundAvailable) {
+      console.log('Value cannot be greter than fund available');
+    } else {
+      this.balanceFund = Number(this.fundAvailable) - Number(searchValue);
+    }
+  }
 }
